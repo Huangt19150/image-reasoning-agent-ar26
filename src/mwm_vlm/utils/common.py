35473fa68d1fb4b3e0203fdddb43b9f7b505da09
Huangt19150 <tianyi.huang.20@ucl.ac.uk>
2026-03-30 22:29:41 +0100

    commit full repo changes

--- src/mwm_vlm/utils/common.py
+++ src/mwm_vlm/utils/common.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 import base64
 import hashlib
 from PIL import Image
@@ -12,3 +14,21 @@ def hash_image(image: Image.Image) -> str:
     This can be used to uniquely identify the image for caching purposes.
     """
     return hashlib.sha256(image.tobytes()).hexdigest()
+
+
+def save_agent_graph_structure(agent_app, save_root_path: str):
+    """
+    Save the agent graph structure as a PNG image.
+    """
+    try:
+        image_bytes = agent_app.get_graph().draw_mermaid_png()
+
+        # Generate unique file name
+        unique_name = datetime.now().strftime("%Y%m%d_%H%M%S")
+        
+        save_path = os.path.join(save_root_path, f"agent_structure_{unique_name}.png")
+        with open(save_path, "wb") as f:
+            f.write(image_bytes)
+        print(f"✅ The agent graph structure has been saved as {save_path}!")
+    except Exception as e:
+        print(f"⚠️ Could not generate PNG image, please check network or dependencies: {e}")
